3fa157b82eedf84be379d816659e3aa394f09d8f
PB220-DEVLOP <pratikbhoyar2002@gmail.com>
2024-04-26 17:59:40 +0530

    rest of the changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
+import { Home } from "./Pages/Home";
 
 function App() {
   return (
     <>
-      <Signup/>
+      {/* <Signup/> */}
+      <Home/>
+
     </>
   );
 }
